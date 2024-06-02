b81c3a65a58f2ee445a441104bd9fbbf2cd91136
Gautam pandit <gautampandit797@gmail.com>
2024-06-02 18:02:21 +0530

    part of the connection of DB

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path: './env'
@@ -13,7 +14,16 @@ dotenv.config({
 
 connectDB()
 
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!! ", err);
+})
 
+// after completion the asynchronous statement it return the promises
 
 
 
