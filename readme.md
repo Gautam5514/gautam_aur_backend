0fc5acec863ae8dc5fb9e01f5e1ef79afd360ee7
Gautam pandit <gautampandit797@gmail.com>
2024-08-19 11:12:48 +0530

    Added the some concept

--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@ Using of the node js and javaScript"# gautam_aur_backend"
 
 
 -> JWT is the bear token Means => if any one wants to the data from me i will send the data! 
+
+MiddleWare => Jane se pahle mujhse mil ke jana, That is middleWare
+
+multer => taking the local storage file and then assign in the local storage
+
+Orinary class -> 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,6 +64,29 @@ userSchema.methods.isPasswordCorrect = async function
 }
 
 // These two are JWT token there is not any differences, Only uses changes
-userSchema.methods.generateAccessToken = async function () {}
-userSchema.methods.generateRefreshToken = async function () {}
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User", userSchema)
