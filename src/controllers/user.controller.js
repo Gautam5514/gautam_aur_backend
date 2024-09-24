0038491fffd72016674c158a740bd959878227eb
Gautam pandit <gautampandit797@gmail.com>
2024-09-24 19:58:52 +0530

    Add watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudnary.js";
 import { apiResponse } from "../utils/apiResponce.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // code for refresh token and access token
 const generateAccessAndRefreshToken = async (userId) => {
@@ -417,6 +418,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) =>{
   )
 })
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(req.user._id)  
+        }
+      },
+      {
+        $lookup: {
+          from: "videos", 
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+            {
+              $lookup: {
+                from: "user",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                  {
+                    $project: {
+                      fullName: 1,
+                      username: 1,
+                      avatar: 1
+                    }
+                  },
+                  {
+                    $addFields: {
+                      owner:{
+                        $first: "$owner"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    ])
+    return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetch successfully"
+      )
+    )
+})
 
 
 
@@ -431,5 +484,6 @@ export {
   updateAccountDetails, 
   updateUserAvatar, 
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
